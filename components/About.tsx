f073e312b267fc07eaa797d09622aa3c624dd4f4
Neonize <20439640+Neonize@users.noreply.github.com>
2023-03-06 17:40:04 +0100

    add About

---
+++ components/About.tsx
@@ -0,0 +1,31 @@
+export default function About() {
+  return (
+    <>
+      <a href="https://vmartens.de">
+        <div className="container max-w-screen-md pl-8 py-4 mx-auto items-center flex flex-col">
+          <h2 className="font-medium text-3xl">
+            Hi i am Vincent, aka Neonize 👋
+          </h2>
+          <ul>
+            <li className="flex p-2">
+              👀 I’m interested in cloud development, automation and devops
+            </li>
+            <li className="flex p-2">
+              🌱 I’m currently learning typescript, go and for my AWS SysOps
+              Administrator Certificate
+            </li>
+            <li className="flex p-2">
+              💞️ I’m looking to collaborate on open source projects in the
+              future
+            </li>
+            <li className="flex p-2">
+              📫 How to reach me? Per mail, message or whatever you find about
+              me.
+            </li>
+          </ul>
+          <p className="flex p-2 underline">More Info on my webpage</p>
+        </div>
+      </a>
+    </>
+  );
+}
---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import AboutComponent from "@/components/About";
+import Layout from "@/sections/Layout";
+
+export default function About() {
+  return (
+    <Layout>
+      <AboutComponent />
+    </Layout>
+  );
+}
