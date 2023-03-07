5a97887f499cb204ee3dac1deb849ea4b7874c6d
Neonize <20439640+Neonize@users.noreply.github.com>
2023-03-07 10:33:38 +0100

    additional pages

---
+++ pages/docs.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/sections/Layout";
+
+export default function DocsPage() {
+  return (
+    <Layout>
+      <div>Docs</div>
+    </Layout>
+  );
+}
---
+++ pages/explanation.tsx
@@ -0,0 +1,10 @@
+import ExplanationElement from "@/components/Explanation";
+import Layout from "@/sections/Layout";
+
+export default function ExplanationPage() {
+  return (
+    <Layout>
+      <ExplanationElement />
+    </Layout>
+  );
+}
