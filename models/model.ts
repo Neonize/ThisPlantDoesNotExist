9818c1b88f6ad2f8da4a8c5ea696be3806ec1864
Neonize <20439640+Neonize@users.noreply.github.com>
2023-04-17 19:54:44 +0200

    Move types to own file

---
+++ models/model.ts
@@ -0,0 +1,25 @@
+export type RequestData = {
+  id?: string;
+};
+
+export type ResponseData = {
+  id: string;
+  status: string;
+  image: string;
+  error: string;
+};
+
+export interface response {
+  id: string;
+  message: string;
+  created: number;
+  apiVersion: string;
+  callID: string;
+  finished: boolean;
+  modelOutputs: modelOutput[];
+}
+export interface modelOutput {
+  images: string[];
+  parameters: any;
+  info: string;
+}
