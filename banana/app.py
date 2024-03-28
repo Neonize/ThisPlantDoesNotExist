f6f2f2257d49e84f9319ea919a3e1ebcd872cc73
Neonize <20439640+Neonize@users.noreply.github.com>
2024-03-28 13:47:29 +0100

    use stableDiffusionXL Pipeline

--- banana/app.py
+++ banana/app.py
@@ -1,8 +1,14 @@
 from io import BytesIO
-from potassium import Potassium, Request, Response
-from diffusers import DiffusionPipeline, DDPMScheduler
 import torch
 import base64
+from potassium import Potassium, Request, Response
+from diffusers import StableDiffusionXLPipeline, UNet2DConditionModel, EulerDiscreteScheduler
+from huggingface_hub import hf_hub_download
+from safetensors.torch import load_file
+
+base = "stabilityai/stable-diffusion-xl-base-1.0"
+repo = "ByteDance/SDXL-Lightning"
+ckpt = "sdxl_lightning_4step_unet.safetensors" # Use the correct ckpt for your step setting!
 
 # create a new Potassium app
 app = Potassium("SDXL-Plants")
@@ -10,19 +16,16 @@ app = Potassium("SDXL-Plants")
 # @app.init runs at startup, and loads models into the app's context
 @app.init
 def init():
-    repo_id="ByteDance/SDXL-Lightning"
+    device = 0 if torch.cuda.is_available() else -1
+    unet = UNet2DConditionModel.from_config(base, subfolder="unet").to("cuda", torch.float16)
+    unet.load_state_dict(load_file(hf_hub_download(repo, ckpt), device=device))
+    pipe = StableDiffusionXLPipeline.from_pretrained(base, unet=unet, torch_dtype=torch.float16, variant="fp16").to("cuda")
 
-    ddpm = DDPMScheduler.from_pretrained(repo_id, subfolder="scheduler")
-    
-    model = DiffusionPipeline.from_pretrained(
-        repo_id, 
-        use_safetensors=True,
-        torch_dtype=torch.float16,
-        scheduler=ddpm
-    ).to("cuda")
+    # Ensure sampler uses "trailing" timesteps.
+    pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
 
     context = {
-        "model": model,
+        "model": pipe,
     }
 
     return context
@@ -38,11 +41,11 @@ def handler(context: dict, request: Request) -> Response:
     image = model(
         prompt=prompt,
         negative_prompt=negative_prompt,
-        guidance_scale=7,
-        num_inference_steps=request.json.get("steps", 20),
+        guidance_scale=0,
+        num_inference_steps=request.json.get("steps", 4),
         generator=torch.Generator(device="cuda").manual_seed(request.json.get("seed")) if request.json.get("seed") else None,
-        width=768,
-        height=512,
+        width=1024,
+        height=1024,
     ).images[0]
 
     buffered = BytesIO()
