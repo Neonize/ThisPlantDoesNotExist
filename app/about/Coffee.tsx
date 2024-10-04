58f44965948b0141dc59149fe2a6d0ad165d4e0d
Neonize <20439640+Neonize@users.noreply.github.com>
2024-10-04 11:26:28 +0200

    Use Next/Script

--- app/about/Coffee.tsx
+++ app/about/Coffee.tsx
@@ -3,7 +3,7 @@ import Script from "next/script";
 export default function Coffee() {
   return (
     <>
-      <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="vmartens" data-description="Support me on Buy me a coffee!" data-color="#40DCA5" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+      <Script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="vmartens" data-description="Support me on Buy me a coffee!" data-color="#40DCA5" data-position="Right" data-x_margin="18" data-y_margin="18"></Script>
     </>
   );
 }
