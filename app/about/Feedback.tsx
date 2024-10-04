68725c26811bd0fe404c88b678848b429a577478
Neonize <20439640+Neonize@users.noreply.github.com>
2024-10-04 10:16:56 +0200

    Update Support

--- app/about/Feedback.tsx
+++
@@ -1,9 +0,0 @@
-import Script from "next/script";
-
-export default function Feedback() {
-  return (
-    <>
-      <Script src="https://portals.co/vrzCZv/widget.js"></Script>
-    </>
-  );
-}
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,11 @@
 import AboutComponent from "@/app/about/About";
 import Coffee from "@/app/about/Coffee";
-import Feedback from "@/app/about/Feedback";
 
 export default function About() {
   return (
     <>
       <AboutComponent />
       <Coffee />
-      <Feedback />
     </>
   );
 }
